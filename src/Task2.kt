def4bb7b72862eb8ca01fd7ca6bde3845e20d012
KasimenkoA <kasimenko@rambler.ru>
2023-04-11 21:53:27 +1000

    Запросить число у пользователя через сканер. Посчитать квадрат этого числа (т.е. умножить число на само себя). Вывести результат на экран.

---
+++ src/Task2.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main() {
+    print("Enter the number: ")
+    val scanner = Scanner(System.`in`)
+    val nn = scanner.nextInt()
+
+    println("Square is ${nn*nn}")
+
+}
