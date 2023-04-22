0d67a2837da790e5c351c21caa4757249e26093f
KasimenkoA <kasimenko@rambler.ru>
2023-04-22 20:24:12 +1000

    Task № 9 Создать массив чисел из 100 элементов. Заполнить массив числами 5 в цикле. В другом цикле вывести значения массива на экран.
    Пример создания массива строк:
    String[] ss = new String[100];
    Пример присвоения:
    ss[4] = "пять";

---
+++ src/Task9.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val nn = 100
+    var list = ArrayList<Int>()
+
+    for (ii in 0 until nn)
+    {
+        list.add(5)
+    }
+
+    for (ii in 0 until nn)
+    {
+        println(list[ii])
+    }
+}
