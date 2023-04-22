85364d55a5d41f0c7fed6f314457440cefb5ba40
KasimenkoA <kasimenko@rambler.ru>
2023-04-22 20:28:23 +1000

    Task № 18 Создать строковый массив mas_c из трех элементов. Заполнить: красный, желтый, белый Создать второй строковый массив mas_f из трех элементов. Заполнить: цветок, квадрат, свет
    Создать цикл fori. Внутри этого цикла создать еще один цикл.
    Внутри вложенного цикла вывести строку: mas_c[i] + " " + mas_f[j]

---
+++ src/Task18.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val mas_c = listOf("red","yellow","white")
+    val mas_f = listOf("flower","square","light")
+
+    for (c in mas_c) {
+        for (f in mas_f) {
+            println("$c $f")
+        }
+    }
+}
