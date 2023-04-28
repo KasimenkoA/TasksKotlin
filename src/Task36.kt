d6ea9a8b2990acf3ff963ca0e7e112cd8eebafd1
KasimenkoA <kasimenko@rambler.ru>
2023-04-28 22:08:44 +1000

    Task № 36 Создать строковый массив месяцев: mon Заполнить. Вывести.

---
+++ src/Task36.kt
@@ -0,0 +1,20 @@
+fun main() {
+    var mon = arrayListOf<String>()
+    mon.add("January")
+    mon.add("February")
+    mon.add("March")
+    mon.add("April")
+    mon.add("May")
+    mon.add("June")
+    mon.add("July")
+    mon.add("August")
+    mon.add("September")
+    mon.add("October")
+    mon.add("November")
+    mon.add("December")
+
+    for (s in mon) {
+        println(s)
+    }
+
+}
