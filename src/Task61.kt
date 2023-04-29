57dd7d66610eaa5e6ff3f32132f298e540d20a04
KasimenkoA <kasimenko@rambler.ru>
2023-04-29 10:19:31 +1000

    Task № 61 Создать объект класса File с путем "Test1.txt". Создать объект класса Scanner с параметром file. В цикле пока, если есть следующая строка в файле, прочитать следующую строку и вывести ее на экран.

---
+++ src/Task61.kt
@@ -0,0 +1,12 @@
+import java.io.File
+import java.util.*
+
+fun main() {
+    val file = File("Test1.txt")
+
+    val scanner: Scanner = Scanner(file)
+    while (scanner.hasNext())
+    {
+        println(scanner.nextLine())
+    }
+}
