5507f452bd12afc325a08fe77249150f29490486
KasimenkoA <kasimenko@rambler.ru>
2023-05-02 21:47:01 +1000

    Task № 74 Нарисовать линию. Нарисовать с помощью линий звезду.

--- src/Draw/MyFrame1.kt
+++ src/Draw/MyFrame1.kt
@@ -7,7 +7,7 @@ import javax.swing.JPanel
 class MyFrame: JFrame()
 {
     init{
-        setSize(600,300)
+        setSize(600,400)
         setLocationRelativeTo(null)
         title = "My frame"
         defaultCloseOperation = EXIT_ON_CLOSE
@@ -19,7 +19,13 @@ class MyPanel: JPanel()
     override fun paintComponent(g: Graphics) {
         super.paintComponent(g)
 
-        g.drawOval(100,150,60,60)
+        //g.drawOval(100,150,60,60)
+
+        g.drawLine(100, 300, 200, 100)
+        g.drawLine(200, 100, 300, 300)
+        g.drawLine(300, 300, 100, 150)
+        g.drawLine(100, 150, 300, 150)
+        g.drawLine(300, 150, 100, 300)
     }
 }
 
