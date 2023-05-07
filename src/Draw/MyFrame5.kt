7a6770a923ac05135bfb08cbb5ed473adba8908c
KasimenkoA <kasimenko@rambler.ru>
2023-05-07 20:56:30 +1000

    Task № 79 В папке Draw создать новый класс MyFrame5. Создать конструктор MyFrame5. Создать новый класс MyPanel5. Нарисовать синюю строку "My string".

---
+++ src/Draw/MyFrame5.kt
@@ -0,0 +1,33 @@
+package Draw
+
+import java.awt.Color
+import java.awt.Graphics
+import javax.swing.JFrame
+import javax.swing.JPanel
+
+class MyFrame5 : JFrame() {
+    init {
+        setSize(600, 400)
+        setLocationRelativeTo(null)
+        title = "My frame"
+        defaultCloseOperation = EXIT_ON_CLOSE
+    }
+}
+
+class MyPanel5 : JPanel() {
+    override fun paintComponent(g: Graphics) {
+        super.paintComponent(g)
+
+        g.color = Color.blue
+        g.drawString("My string", 50, 100)
+    }
+}
+
+fun main() {
+    val myFrame = MyFrame5()
+
+    val myPanel = MyPanel5()
+    myFrame.add(myPanel)
+
+    myFrame.isVisible = true
+}
