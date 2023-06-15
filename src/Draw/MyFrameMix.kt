91f726b74dc15d433735925322f6ef4d35f62bb1
KasimenkoA <kasimenko@rambler.ru>
2023-06-15 22:29:14 +1000

    Task № 118 Цель: с помощью мышки нарисовать круг на panelDraw. В класс MyFrameMix добавить глобальные переменные int startX;         int startY;         int endX;         int endY; Инициализировать их в конструкторе. Создать класс MyMouseAdapter, расширенный от MouseAdapter. В нем переопределить методы: mousePressed, mouseReleased. При нажатии прописывать значения startX и startY. При отпускании endX и endY.
    Создать метод drawFigure()
    В котором определить минимальные и максимальные значения X и Y.
    Получить из panelDraw объект класса Graphics2D.
    Нарисовать в нем Ellipse2D.
    
    В конструктор флейма добавить метод:
    panelDraw.addMouseListener( new MyMouseAdapter());

--- src/Draw/MyFrameMix.kt
+++ src/Draw/MyFrameMix.kt
@@ -1,9 +1,9 @@
 package Draw
 
-import java.awt.BorderLayout
-import java.awt.Color
-import java.awt.Dimension
-import java.awt.GridLayout
+import java.awt.*
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import java.awt.geom.Ellipse2D
 import javax.swing.JButton
 import javax.swing.JFrame
 import javax.swing.JPanel
@@ -11,6 +11,10 @@ import javax.swing.JPanel
 class MyFrameMix : JFrame() {
     var panelDraw: PanelDrawMix
     var button1: JButton
+    var startX = 0
+    var startY = 0
+    var endX = 0
+    var endY = 0
 
     init {
         title = "My frame Mix"
@@ -23,6 +27,7 @@ class MyFrameMix : JFrame() {
 
         panelDraw = PanelDrawMix()
         panelDraw.preferredSize = Dimension(600, 400)
+        panelDraw.addMouseListener(MyMouseAdapter())
 
         button1 = JButton("Button 1")
         button1.addActionListener { panelDraw.fillBackground() }
@@ -30,9 +35,42 @@ class MyFrameMix : JFrame() {
 
         add(panelButton, BorderLayout.NORTH)
         add(panelDraw, BorderLayout.SOUTH)
+
+        startX = 0
+        startY = 0
+        endX = 0
+        endY = 0
     }
 
+    inner class MyMouseAdapter : MouseAdapter() {
+        override fun mousePressed(e: MouseEvent) {
+            startX = e.x
+            startY = e.y
+        }
+
+        override fun mouseReleased(e: MouseEvent) {
+            endX = e.x
+            endY = e.y
+            drawFigure()
+        }
+    }
+
+    private fun drawFigure() {
+        val minX: Int = Math.min(startX, endX)
+        val minY: Int = Math.min(startY, endY)
+        val maxX: Int = Math.max(startX, endX)
+        val maxY: Int = Math.max(startY, endY)
+
+        val g2 = panelDraw.graphics as Graphics2D
+        val ellipse2D: Ellipse2D = Ellipse2D.Double(
+            minX.toDouble(), minY.toDouble(), (maxX - minX).toDouble(),
+            (maxY - minY).toDouble()
+        )
+        g2.draw(ellipse2D)
+    }
 }
+
+
 
 
 class PanelDrawMix : JPanel() {
