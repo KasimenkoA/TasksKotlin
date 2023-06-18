4b45e56b41d72df33c0f19711f5dcc451c3ef3ee
KasimenkoA <kasimenko@rambler.ru>
2023-06-18 21:50:38 +1000

    Task № 121 Реализовать рисование фигур выделенным цветом. Для этого: Создать private class MyShape с переменными Shape и Color. Создать конструктор. Изменить типы tempShape и shapes для работы с MyShape. Создать метод getMyShape в котором создать объектк MyShape c curColor и фигурой getFigure(). Переделать метод paint класса PanelDraw, учитывая, что tempShape и shapes содержат объкты типа MyShape.
    Task № 120
    Добавить общую переменную Color curColor и инициализировать ее цветом Белый.
    Добавить кнопку 2.
    По нажатии на которую изменить цвет curColor и цвет самой кнопки.
    Для этого в PanelDraw создать метод selectColor.

--- src/Draw/MyFrameMix.kt
+++ src/Draw/MyFrameMix.kt
@@ -7,14 +7,17 @@ import java.awt.event.MouseMotionAdapter
 import java.awt.geom.Ellipse2D
 import java.util.*
 import javax.swing.JButton
+import javax.swing.JColorChooser
 import javax.swing.JFrame
 import javax.swing.JPanel
 
 class MyFrameMix : JFrame() {
     var panelDraw: PanelDrawMix
     var button1: JButton
-    var tempShape: Shape? = null
-    var shapes = ArrayList<Shape>()
+    var button2: JButton
+    var tempShape: MyShape
+    var curColor: Color
+    var shapes = ArrayList<MyShape>()
     var startX = 0
     var startY = 0
     var endX = 0
@@ -27,7 +30,7 @@ class MyFrameMix : JFrame() {
         defaultCloseOperation = EXIT_ON_CLOSE
 
         val panelButton = JPanel()
-        panelButton.layout = GridLayout(2, 4)
+        panelButton.layout = GridLayout(1, 4)
 
         panelDraw = PanelDrawMix()
         panelDraw.preferredSize = Dimension(600, 400)
@@ -38,6 +41,10 @@ class MyFrameMix : JFrame() {
         button1.addActionListener { panelDraw.fillBackground() }
         panelButton.add(button1)
 
+        button2 = JButton("Color")
+        button2.addActionListener { panelDraw.selectColor() }
+        panelButton.add(button2)
+
         add(panelButton, BorderLayout.NORTH)
         add(panelDraw, BorderLayout.SOUTH)
 
@@ -45,6 +52,9 @@ class MyFrameMix : JFrame() {
         startY = 0
         endX = 0
         endY = 0
+
+        curColor = Color.WHITE
+        tempShape = MyShape(curColor,getFigure())
     }
 
     inner class MyMouseAdapter : MouseAdapter() {
@@ -56,7 +66,7 @@ class MyFrameMix : JFrame() {
         override fun mouseReleased(e: MouseEvent) {
             endX = e.x
             endY = e.y
-            getFigure()
+            shapes.add(getMyShape())
         }
     }
 
@@ -64,7 +74,7 @@ class MyFrameMix : JFrame() {
         override fun mouseDragged(e: MouseEvent) {
             endX = e.x
             endY = e.y
-            tempShape = getFigure()
+            tempShape = getMyShape()
             repaint()
         }
     }
@@ -81,24 +91,35 @@ class MyFrameMix : JFrame() {
         )
     }
 
+    private fun getMyShape(): MyShape {
+        return MyShape(curColor, getFigure())
+    }
+
+    class MyShape(var color: Color, var shape: Shape)
+
     inner class PanelDrawMix : JPanel() {
         override fun paint(g: Graphics) {
             val g2 = g as Graphics2D
             if (tempShape != null) {
-                g2.draw(tempShape)
+                g2.color = tempShape.color
+                g2.draw(tempShape.shape)
             }
             for (shape in shapes) {
-                g2.draw(shape)
+                g2.color = shape.color
+                g2.draw(shape.shape)
             }
         }
 
         fun fillBackground() {
             background = Color.PINK
         }
+
+        fun selectColor() {
+            curColor = JColorChooser.showDialog(null, "Select color", Color.WHITE)
+            button2.background = curColor
+        }
     }
 }
-
-
 
 
 fun main() {
