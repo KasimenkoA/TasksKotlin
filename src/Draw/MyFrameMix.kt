4cfd1adfce82df97e411a6d11a783c9599f2e9f9
KasimenkoA <kasimenko@rambler.ru>
2023-06-24 22:35:17 +1000

    Task № 126 Цель рисовать выбранную в выпадающем списке фигуру. Для в методе getFigure() получить строковую переменную shapeType из jComboBox. Нарисовать и возвратить выбранную фигуру.

--- src/Draw/MyFrameMix.kt
+++ src/Draw/MyFrameMix.kt
@@ -4,7 +4,10 @@ import java.awt.*
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import java.awt.event.MouseMotionAdapter
+import java.awt.geom.Arc2D
 import java.awt.geom.Ellipse2D
+import java.awt.geom.Line2D
+import java.awt.geom.Rectangle2D
 import java.util.*
 import javax.swing.*
 
@@ -14,7 +17,7 @@ class MyFrameMix : JFrame() {
     var button2: JButton
     var tempShape: MyShape
     var curColor: Color
-    var checkBox1: JCheckBox = JCheckBox( "fill", false )
+    var checkBox1: JCheckBox = JCheckBox("fill", false)
     var jTextField: JTextField = JTextField()
     var jComboBox: JComboBox<String> = JComboBox()
 
@@ -46,7 +49,7 @@ class MyFrameMix : JFrame() {
         button2.addActionListener { panelDraw.selectColor() }
         panelButton.add(button2)
 
-        panelButton.add( checkBox1 )
+        panelButton.add(checkBox1)
 
         panelButton.add(jTextField)
 
@@ -62,7 +65,7 @@ class MyFrameMix : JFrame() {
         endX = 0
         endY = 0
 
-        curColor = Color.WHITE
+        curColor = Color.BLACK
         tempShape = MyShape()
     }
 
@@ -90,14 +93,25 @@ class MyFrameMix : JFrame() {
 
 
     private fun getFigure(): Shape {
-        val minX = Math.min(startX, endX)
-        val minY = Math.min(startY, endY)
-        val maxX = Math.max(startX, endX)
-        val maxY = Math.max(startY, endY)
-        return Ellipse2D.Double(
-            minX.toDouble(), minY.toDouble(), (maxX - minX).toDouble(),
-            (maxY - minY).toDouble()
-        )
+        val minX = Math.min(startX, endX).toDouble()
+        val minY = Math.min(startY, endY).toDouble()
+        val maxX = Math.max(startX, endX).toDouble()
+        val maxY = Math.max(startY, endY).toDouble()
+
+        var shapeType = ""
+        if (jComboBox.selectedItem != null) {
+            shapeType = jComboBox.selectedItem as String
+        }
+
+        return when (shapeType)
+                        {
+                            "oval" -> Ellipse2D.Double(minX, minY, maxX - minX, maxY - minY )
+                            "rectangle" -> Rectangle2D.Double( minX, minY, maxX - minX, maxY - minY )
+                            "arc" -> Arc2D.Double( minX, minY, maxX - minX, maxY - minY, 0.0, 90.0, Arc2D.PIE )
+
+            else -> Line2D.Double( minX, minY, maxX, maxY )
+        }
+
     }
 
     private fun getMyShape(): MyShape {
@@ -125,17 +139,17 @@ class MyFrameMix : JFrame() {
         override fun paint(g: Graphics) {
             val g2 = g as Graphics2D
 
-            if (tempShape != null) {
-                g2.color = tempShape.color
-                if (tempShape.lineWidth > 0) {
-                    g2.stroke = BasicStroke(tempShape.lineWidth.toFloat())
-                }
-                if (tempShape.isFill) {
-                    g2.fill(tempShape.shape)
-                } else {
-                    g2.draw(tempShape.shape)
-                }
+
+            g2.color = tempShape.color
+            if (tempShape.lineWidth > 0) {
+                g2.stroke = BasicStroke(tempShape.lineWidth.toFloat())
             }
+            if (tempShape.isFill) {
+                g2.fill(tempShape.shape)
+            } else {
+                g2.draw(tempShape.shape)
+            }
+
             for (shape in shapes) {
                 g2.color = shape.color
                 if (shape.lineWidth > 0) {
