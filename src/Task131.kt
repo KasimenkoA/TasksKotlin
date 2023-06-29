4d63b2b8af5b263aea431505e4eb8c57a6325396
KasimenkoA <kasimenko@rambler.ru>
2023-06-29 22:35:16 +1000

    Task № 131 Создать класс "Dog" с переменными "name" (имя), "breed" (порода) и "age" (возраст в месяцах). Реализовать метод, который проверяет, является ли собака щенком (младше 12 месяцев). Создать три собаки. Вывести информацию по ним.

---
+++ src/Task131.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val dog1 = Dog("Bobic", "boxer", 7)
+    val dog2 = Dog("Tarzan", "setter", 37)
+    val dog3 = Dog("Mavr", "diver", 28)
+
+    dog1.showInfo()
+    dog2.showInfo()
+    dog3.showInfo()
+
+}
+
+internal class Dog(var name: String, var breed: String, var age: Int) {
+    val isPuppy: Boolean
+        get() = age < 12
+
+    fun showInfo() {
+        println("Dog: $name, breed: $breed, age: $age.")
+        if (isPuppy) {
+            println("It is puppy.")
+        } else {
+            println("It's not a putty.")
+        }
+    }
+}
