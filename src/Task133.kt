ba70c77bfea353e3d6de07761ce246bf3c384627
KasimenkoA <kasimenko@rambler.ru>
2023-07-01 22:14:35 +1000

    Task № 133 Создать класс "Movie" с переменными "title" (название), "genre" (жанр) и "length" (продолжительность) в минутах. Создать метод showInfo. В котором вывести информацию о фильме, причем продолжительность нужно вывести в часах и минутах.

---
+++ src/Task133.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+    val movie1 = Movie("Undefeated", "drama", 113)
+    val movie2 = Movie("Hobbit", "fantasy", 209)
+
+    movie1.showInfo()
+    movie2.showInfo()
+
+}
+
+internal class Movie(val title: String, val genre: String, val length: Int) {
+    val lengthHour: String
+        get() {
+            val hour = length / 60
+            val min = length - hour * 60
+            return "$hour hours, $min min."
+        }
+
+    fun showInfo() {
+        println("Film: $title, genre: $genre, length: $lengthHour")
+    }
+}
