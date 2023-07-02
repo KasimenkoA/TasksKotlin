e88c328e5f5ad6f89bc151747c16726eda2d51ef
KasimenkoA <kasimenko@rambler.ru>
2023-07-02 22:09:02 +1000

    Task № 134 Создать класс "BankAccount" с переменными "accountNumber" (номер счета), "balance" (баланс) и "ownerName" (имя владельца). Реализовать методы для внесения и снятия денег со счета, а также для проверки баланса. addMoney - пополнить деньги takeMoney - снять деньги

---
+++ src/Task134.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val bankAccount = BankAccount("125", "Alexey", 1000)
+    bankAccount.addMoney(23)
+    bankAccount.takeMoney(10)
+    bankAccount.showRestMoney()
+}
+
+internal class BankAccount(var accountNumber: String, var ownerName: String, var balance: Int) {
+    fun showRestMoney() {
+        println("Total money: $balance")
+    }
+
+    fun addMoney(mm: Int) {
+        balance += mm
+        println("Add: $mm")
+        showRestMoney()
+    }
+
+    fun takeMoney(mm: Int) {
+        balance -= mm
+        println("Take: $mm")
+        showRestMoney()
+    }
+}
