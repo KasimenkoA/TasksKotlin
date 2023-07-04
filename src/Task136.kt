731a750175bc43f8eff97c8026ff82d2e7583b2e
KasimenkoA <kasimenko@rambler.ru>
2023-07-04 21:28:13 +1000

    Task № 136 Создать класс Cat (кот) с закрытым (private) полем "name" и полем age (возраст). С открытым (public) полем isSleeping (спит) (boolean). Написать метод setAge, который принимает целочисленный параметр и устанавливает значение поля age. Написать метод getAge, который возвращает значение поля age. Написать метод showInfo. Создать кота. Вывести информацию о возрасте. Установить, что он спит. Вывести информацию. Установить, что он не спит. Вывести информацию.

---
+++ src/Task136.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val cat = Cat("Barsic", 4)
+
+    println(cat.age)
+
+    cat.isSleeping = true
+    cat.showInfo()
+
+    cat.isSleeping = false
+    cat.showInfo()
+}
+
+internal class Cat(private val name: String, var age: Int) {
+    var isSleeping = false
+    fun showInfo() {
+        println("Cat: $name, age: $age, is sleeping: $isSleeping")
+    }
+}
