6669855a0c0bcca7c61ba4477d1b8ebd47549d6a
KasimenkoA <kasimenko@rambler.ru>
2023-07-05 21:35:54 +1000

    Task № 137 Создать класс MiniCar (автомобиль) с закрытым (private) полем make (марка) и с открытым (public) методом drive (едь). Написать метод drive, который выводит на экран сообщение "I am driving a ..." (я еду на ...). Создать объект класса MiniCar и вызвать метод drive.

---
+++ src/Task137.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val miniCar = MiniCar("Citroen")
+    miniCar.drive()
+}
+
+internal class MiniCar(private val make: String) {
+    fun drive() {
+        println("I'm driving a $make.")
+    }
+}
