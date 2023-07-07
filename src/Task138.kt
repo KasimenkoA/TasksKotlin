fe220521a67bac1c9c3494dd30e5d0acfac2c4fe
KasimenkoA <kasimenko@rambler.ru>
2023-07-07 21:50:02 +1000

    Task № 138 Создай класс PetAnimal (домашнее животное) с закрытым (private) полем name и с открытым (public) методом makeSound (издавать звук). Напиши метод makeSound, который выводит на экран сообщение "The " + Имя животного + " makes a sound" (животное издает звук). Создай объект класса Animal и вызови метод makeSound.

---
+++ src/Task138.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val petAnimal = PetAnimal("Cow")
+    petAnimal.makeSound()
+}
+
+internal class PetAnimal(private val name: String) {
+    fun makeSound() {
+        println("The $name makes a sound")
+    }
+}
