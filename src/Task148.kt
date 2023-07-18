38dbb363426348083cdcf822ec7969541dfe0858
KasimenkoA <kasimenko@rambler.ru>
2023-07-18 22:06:41 +1000

    Task № 148 Задача о паролях: Напиши программу, которая будет запрашивать у пользователя пароль и продолжать запрашивать его, пока он не введет верный пароль. Правильный пароль - "password".

---
+++ src/Task148.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main() {
+    var pas = ""
+    while (pas != "password") {
+        print("Input string: ")
+        pas = readln()
+    }
+    println("Password is correct!")
+}
