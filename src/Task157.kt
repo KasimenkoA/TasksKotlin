3de8cdef2c88c57878ef4bb1b520747ea84b2ecf
KasimenkoA <kasimenko@rambler.ru>
2023-07-29 21:46:20 +1000

    Task № 157 Создайте массив строк типа String из 6 элементов и добавьте в него названия фруктов. Выведите все элементы массива в верхнем регистре (большими буквами). Для вывода использовать цикл for each. Пример: for (int ii: mas) { ... }

---
+++ src/Task157.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val mas = arrayOfNulls<String>(6)
+    mas[0] = "Apple"
+    mas[1] = "Apricot"
+    mas[2] = "Avocado"
+    mas[3] = "Blackberry"
+    mas[4] = "Banana"
+    mas[5] = "Orange"
+
+    for (ss in mas) {
+        println(ss)
+    }
+}
