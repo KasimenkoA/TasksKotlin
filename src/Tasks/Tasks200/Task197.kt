75ad97a4a63808bfa511959ee66a5aff31299c04
KasimenkoA <kasimenko@rambler.ru>
2023-09-07 22:06:22 +1000

    Task № 197 Метод Character.toLowerCase() Запросите у пользователя строку. Выделите 2 символ строки и с помощью команды Character.toLowerCase() переведите символ в нижний регистр.

---
+++ src/Tasks/Tasks200/Task197.kt
@@ -0,0 +1,12 @@
+package Tasks.Tasks200
+
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    print("Input any string: ")
+    val ss = scanner.nextLine()
+
+    val ch = ss[1]
+    println(Character.toLowerCase(ch))
+}
