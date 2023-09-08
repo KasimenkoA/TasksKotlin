24ec623b5146a54d49a539c4e31838b48a541e73
KasimenkoA <kasimenko@rambler.ru>
2023-09-08 21:51:53 +1000

    Task № 198 Метод compareTo() - сравнивает одну строку с другой, возвращает 0, если строки равны. В цикле while запрашивать у пользователя строку. Если пользователь вводит "exit", то выйти из цикла.

---
+++ src/Tasks/Tasks200/Task198.kt
@@ -0,0 +1,14 @@
+package Tasks.Tasks200
+
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    var ss: String
+
+    while (true) {
+        print("Input string: ")
+        ss = scanner.nextLine()
+        if (ss.compareTo("exit") == 0) break
+    }
+}
