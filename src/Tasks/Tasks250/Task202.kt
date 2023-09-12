2e44fc69edd5bcc28e08d790122e8a0407e7b463
KasimenkoA <kasimenko@rambler.ru>
2023-09-12 21:58:45 +1000

    Task № 202 Метод trim() - обрезать - убирает пробелы в начале и в конце строки. Запросите у пользователя строку. Выведите длину строки. Обрежьте введенную строку. Выведите строку. Выведите длину строки.

---
+++ src/Tasks/Tasks250/Task202.kt
@@ -0,0 +1,15 @@
+package Tasks.Tasks250
+
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    print("Input string: ")
+    val ss = scanner.nextLine()
+
+    println(ss.length)
+
+    val ss2 = ss.trim()
+    println(ss2)
+    println(ss2.length)
+}
