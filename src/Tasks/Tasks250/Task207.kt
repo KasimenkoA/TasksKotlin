49ce101be890a92369271357405c4c91c3040d86
KasimenkoA <kasimenko@rambler.ru>
2023-09-17 21:42:07 +1000

    Task № 207 Создайте переменные a и b типа int. С помощью собственной функции расчитайте сумму этих переменных.
    Пример функции:
    private static int ИМЯ_ФУНКЦИИ( int a, int b )
                {
                    return РЕЗУЛЬТА;
                }

---
+++ src/Tasks/Tasks250/Task207.kt
@@ -0,0 +1,14 @@
+package Tasks.Tasks250
+
+fun main() {
+    val a = 5
+    val b = 4
+    val c: Int = sum(a, b)
+
+    println(c)
+
+}
+
+private fun sum(a: Int, b: Int): Int {
+    return a + b
+}
