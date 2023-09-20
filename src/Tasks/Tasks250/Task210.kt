a094acb1b93f4f618222f07abad881123bce0210
KasimenkoA <kasimenko@rambler.ru>
2023-09-20 21:41:10 +1000

    Task № 210 Запросите имя и возраст пользователя. Передайте имя и возраст в функцию showInfo. В этой функции выведите на печать информацию о пользователе. Напр. Тебя зовут Петя, тебе 12 лет.

---
+++ src/Tasks/Tasks250/Task210.kt
@@ -0,0 +1,15 @@
+package Tasks.Tasks250
+
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    print("Input your name: ")
+    val ss1 = scanner.nextLine()
+
+    print("Input your age: ")
+    val ss2 = scanner.nextLine()
+
+    println("Your name is $ss1, you are $ss2 years old.")
+}
