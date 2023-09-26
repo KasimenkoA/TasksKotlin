f07ea422da94634267a927653d3793e8defee95e
KasimenkoA <kasimenko@rambler.ru>
2023-09-26 22:12:51 +1000

    Task № 215 Создайте класс Monkey (обезьяна) с реквизитом name. Создайте три обезьяны с разными именами. Выведите имена всех обезьян.

---
+++ src/Tasks/Tasks250/Task215.kt
@@ -0,0 +1,13 @@
+package Tasks.Tasks250
+
+class Monkey(var name: String)
+
+fun main() {
+    val monkey1 = Monkey("Chi-Chi")
+    val monkey2 = Monkey("Saya")
+    val monkey3 = Monkey("Taya")
+
+    println(monkey1.name)
+    println(monkey2.name)
+    println(monkey3.name)
+}
