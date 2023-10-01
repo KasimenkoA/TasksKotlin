3ad1106f1172721f21bcbbad2e39a9bf543ea22c
KasimenkoA <kasimenko@rambler.ru>
2023-10-01 21:45:58 +1000

    Task № 218 В предыдущее задание добавить класс SuperFox, который нужно наследовать от класса Fox командой extends. Перепишите (Override) в нем метод: showInfo(), в который добавить строку: "I am super Fox!". Создайте три супер лисы. Выведите по ним информацию.

--- src/Tasks/Tasks250/Task217.kt
+++ src/Tasks/Tasks250/Task217.kt
@@ -1,8 +1,15 @@
 package Tasks.Tasks250
 
-internal class Fox(var name: String) {
-    fun showInfo() {
+internal open class Fox(var name: String) {
+    open fun showInfo() {
         println("Fox - $name")
+    }
+}
+
+internal class SuperFox(name: String) : Fox(name) {
+    override fun showInfo() {
+        super.showInfo()
+        println("I am super Fox!")
     }
 }
 
@@ -14,4 +21,13 @@ fun main() {
     fox1.showInfo()
     fox2.showInfo()
     fox3.showInfo()
+
+    println()
+    val sfox1 = SuperFox("Elza")
+    val sfox2 = SuperFox("Nata")
+    val sfox3 = SuperFox("Mura")
+
+    sfox1.showInfo()
+    sfox2.showInfo()
+    sfox3.showInfo()
 }
