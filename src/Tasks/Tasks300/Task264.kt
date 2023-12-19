659d76ca018b40733d7f31a056f4db2b0ccf904c
KasimenkoA <kasimenko@rambler.ru>
2023-12-19 22:52:05 +1000

    Task № 264 Создайте класс Plane264 с полями name и speed. Создайте конструктор, создайте геттеры. В функции main создайте коллекцию ArrayList из самолетов. В цикле добавьте в коллекцию 10 самолетов. Имя должно быть: Самолет1, Самолет2 и т.д. Скорость должна быть случайным числом до 8000.
    В цикле foreach выведите информацию о каждом самолете вида:
    Самолет1, скорость 2500

---
+++ src/Tasks/Tasks300/Task264.kt
@@ -0,0 +1,29 @@
+package Tasks.Tasks300
+
+import java.util.*
+
+internal class Plane264(name: Int, speed: Int) {
+    val name: String
+    val speed: Int
+
+    init {
+        this.name = "Plane$name"
+        this.speed = speed
+    }
+}
+
+fun main() {
+
+    val arrayList = ArrayList<Plane264>()
+    val nn: Int
+    val random = Random()
+
+    for (i in 1..10) {
+        arrayList.add(Plane264(i, random.nextInt(8000)))
+    }
+
+    for (plane264 in arrayList) {
+        println("" + plane264.name + ", speed: " + plane264.speed)
+    }
+
+}
