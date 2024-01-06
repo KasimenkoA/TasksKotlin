2e55f7f503873a5a910fa711adb0642ae41a155c
KasimenkoA <kasimenko@rambler.ru>
2024-01-06 22:34:38 +1000

    Task № 271 Запросить у пользователя число (X) и выдать результат (Y) по формуле: Y = X + X*X

---
+++ src/Tasks/Tasks300/Task271.kt
@@ -0,0 +1,14 @@
+package Tasks.Tasks300
+
+import java.util.*
+
+fun main() {
+
+    val scanner = Scanner(System.`in`)
+    print("Input number: ")
+    val X = scanner.nextInt()
+
+    val Y = X + X * X
+    println(Y)
+
+}
